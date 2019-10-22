2af3e8b70422e2bc2873380fec22dc9f6dcd572f
Chris McMenomy <chris.mcmenomy@gmail.com>
2019-10-22 17:32:05 -0500

    Main functionality complete. Cleaned up and added readme/docs.

--- server.js
+++ server.js
@@ -2,67 +2,104 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-// use compare versions plugin, but also implement on my own
-
-// orchestrator func called by endpoint
-// others that do the work of comparison
-// handle any string, not just software versions
-
-const compareValidVersionNumbers = (version1, version2) => {
-  // if both valid versions, split them both and iterate through to compare
-  // make sure to check the are the same length
-
-  const [v1Segments, v2Segments] = [segmentVersionString(version1), segmentVersionString(version2)];
-  for (let i = 0; i < segmentVersionString(version1).length; i++) {
-    const v1SegmentsEnded = v1Segments[i] === undefined;
-    const v2SegmentsEnded = v2Segments[i] === undefined;
-    if (v1SegmentsEnded) {
-      if(+v2Segments[i] > 0) {return 'Before'};
-    } else if (v2SegmentsEnded) {
-      if(+v1Segments[i] > 0) {return 'After'};
-    } else if (+v1Segments[i] > +v2Segments[i]) {
-      return 'After';
-    } else if (+v1Segments[i] < +v2Segments[i]) {
-      return 'Before';
-    }
-  };
-  return 'Equal';
-};
-
-const compareStringsAsVersionNumbers = (string1, string2) => {
-  // if they aren't valid, remove all spaces and periods and compare step by step in order of ascii value
-
-};
-
+/**
+ * Confirm that a supplied string is a valid software verson accoriding to the
+ * major.minor.patch pattern.
+ * @param {string} string The string to validate.
+ */
 const validSoftwareVersion = (string) => {
   const versionRegex = /^\d{1,2}((\.\d{1,2}){0,2})?$/;
   return versionRegex.test(string);
 };
 
-const segmentVersionString = (string) => {
-  return string.split('.');
+/**
+ * Helper function to add back decimal points on minor and patch segments before
+ * converting to a number, in order to preserve leading zeros when comparing.
+ * @param {string} segmentString The segment to parse.
+ * @param {number} index Index of the loop.
+ */
+const parseSegment = (segmentString, index) => {
+  return index != 0 ? +`.${segmentString}` : +segmentString;
 };
 
+/**
+ * Determine whether a valid software version number came before, after, or is equal to another.
+ * @param {string} version1 The first version string.
+ * @param {string} version2 The second version string.
+ */
+const compareValidVersionNumbers = (version1, version2) => {
+  const [v1Segments, v2Segments] = [version1.split('.'), version2.split('.')];
+  // Iterate through the version segments and compare.
+  for (let i = 0; i < v1Segments.length; i++) {
+    // Check for lack of additional segments (minor version, patch).
+    if (v2Segments[i] === undefined) {
+      // Check to see if the additional segment actually differentiates the versions.
+      if(parseSegment(v1Segments[i], i) > 0) {return 'After'};
+    } else if (parseSegment(v1Segments[i], i) > parseSegment(v2Segments[i], i)) {
+      return 'After';
+    } else if (parseSegment(v1Segments[i], i) < parseSegment(v2Segments[i], i)) {
+      return 'Before';
+    }
+  };
+  // Handle the case where the second version number is longer than the first.
+  if (v2Segments.length > v1Segments.length && +v2Segments[v1Segments.length] > 0) {
+    return 'Before';
+  } else {
+    return 'Equal';
+  };
+};
+
+/**
+ * Compare two strings that are not valid version numbers, in the same way that
+ * version numbers would be. Decimal points and whitespace are removed from the supplied
+ * strings and are compared character by character based on ASCII value.
+ * @param {string} string1 The first string.
+ * @param {string} string2 The second string.
+ */
+const compareStringsAsVersionNumbers = (string1, string2) => {
+  let scrubbedString1 = string1.replace(/[ \.]/g, '').split('');
+  let scrubbedString2 = string2.replace(/[ \.]/g, '').split('');
+  // Iterate through the strings character by character to compare ascii values.
+  for (let i = 0; i < scrubbedString1.length; i++) {
+    // If string one is longer, it is automatically 'after' the second.
+    if (scrubbedString2[i] === undefined) {
+      return 'After';
+    } else if (scrubbedString1[i].charCodeAt(0) > scrubbedString2[i].charCodeAt(0)) {
+      return 'After';
+    } else if (scrubbedString1[i].charCodeAt(0) < scrubbedString2[i].charCodeAt(0)) {
+      return 'Before';
+    }
+  };
+  // If string two is longer, it is automatically 'after' the first.
+  if (scrubbedString2.length > scrubbedString1.length) {
+    return 'Before';
+  } else {
+    return 'Equal';
+  };
+};
+
+/**
+ * Orchestrator function for handling the comparison of two passed in strings
+ * as software version numbers.
+ * @param {*} req The incoming request.
+ * @param {*} res The outgoing response.
+ */
 const compareStrings = (req, res) => {
   const firstString = req.query.first;
   const secondString = req.query.second;
   let result;
 
-  if (validSoftwareVersion(firstString) && validSoftwareVersion(secondString)) {
-    result = compareValidVersionNumbers(firstString, secondString);
+  // Check that two strings were received.
+  if (!firstString || !secondString) {
+    result = 'Please provide two strings to compare via query parameters. Thank you!';
   } else {
-    result = compareStringsAsVersionNumbers(firstString, secondString);
-  }
-
-
-
-  res.send( JSON.stringify({
-      first: segmentVersionString(firstString),
-      second: segmentVersionString(secondString),
-      valid1: validSoftwareVersion(firstString),
-      valid2: validSoftwareVersion(secondString),
-      result: result
-  }));
+    if (validSoftwareVersion(firstString) && validSoftwareVersion(secondString)) {
+      result = compareValidVersionNumbers(firstString, secondString);
+    } else {
+      result = compareStringsAsVersionNumbers(firstString, secondString);
+    };
+  };
+  res.send(result);
 };
 
 app.get('/comparatron', compareStrings);
